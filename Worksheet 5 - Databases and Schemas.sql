fcaad66bbe9ba603e0988b226692c641cf8f959c
profadityasaxena <iam_adisxn@outlook.com>
2025-06-09 15:02:49 -0400

    added databases and schemas

---
+++ Worksheet 5 - Databases and Schemas.sql
@@ -0,0 +1,30 @@
+
+show warehouses
+
+DROP DATABASE test_database;
+
+CREATE or REPLACE DATABASE test_database
+
+SHOW DATABASES;
+
+DROP DATABASE test_database;
+
+UNDROP DATABASE test_database;
+
+DROP DATABASE test_database2;
+
+CREATE DATABASE test_database2;
+
+USE DATABASE test_database2;
+
+USE DATABASE test_database;
+
+CREATE SCHEMA test_schema;
+
+SHOW SCHEMAS;
+
+DESCRIBE DATABASE test_database;
+
+DROP SCHEMA test_schema;
+
+UNDROP SCHEMA test_schema;
